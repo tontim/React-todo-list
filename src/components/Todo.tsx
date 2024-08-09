bc2fec2b085bd7f17bede3a50a2f9a30a91fb3fa
tontim <tontimon+dev@gmail.com>
2024-08-09 18:31:27 +0200

    add todo working logic

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -21,14 +21,52 @@ export function TodoList() {
         done: false,
     });
 
+    const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setNewTodo((prev) => ({ ...prev, [name]: value }));
+      };
+    
+      const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (newTodo.name && newTodo.description) {
+          const updatedTodo = {
+            ...newTodo,
+            id: Date.now().toString(),
+          };
+          setTodos((prevTodos) => [...prevTodos, updatedTodo]);
+          setNewTodo({ id: '', name: '', description: '', done: false }); // 
+        }
+      };
+
     return (
-        <div className="container">
-            <h2>Todo List</h2>
-            <ul>
-                {todos.map((todo) => (
-                    <TodoItem key={todo.id} todo={todo} />
-                ))}
-            </ul>
-        </div>
+    <div className="container">
+      <h2>Todo List</h2>
+      
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="name"
+          value={newTodo.name}
+          onChange={handleInputChange}
+          placeholder="Todo name"
+        />
+        <input
+          type="text"
+          name="description"
+          value={newTodo.description}
+          onChange={handleInputChange}
+          placeholder="Todo description"
+        />
+        <button type="submit">
+          Add Todo
+        </button>
+      </form>
+      
+      <ul>
+        {todos.map((todo) => (
+          <TodoItem key={todo.id} todo={todo} />
+        ))}
+      </ul>
+    </div>
     );
 }
